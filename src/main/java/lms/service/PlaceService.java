4711a674e112409e86180659430899ffdd701af5
Schmoopy93 <38230581+Schmoopy93@users.noreply.github.com>
2019-05-09 23:24:01 +0200

    added TitleController

--- src/main/java/lms/service/PlaceService.java
+++ src/main/java/lms/service/PlaceService.java
@@ -1,12 +1,8 @@
 package lms.service;
-
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import lms.domain.Outcome;
 import lms.domain.Place;
 import lms.repository.PlaceRepository;
 
