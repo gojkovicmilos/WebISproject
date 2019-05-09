b5ff78a8bca70d2a5ad4395ad60037ff1a5eb746
emil <emil@debian>
2019-05-09 21:10:35 +0200

    aofjaf

--- src/main/java/lms/repository/AddressRepository.java
+++ src/main/java/lms/repository/AddressRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import lms.domain.Address;
-
+// cao
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long>{
 
