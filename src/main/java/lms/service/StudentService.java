0847cbff1462a4b99c1356f824efbd98fc017fe6
Schmoopy93 <38230581+Schmoopy93@users.noreply.github.com>
2019-05-09 22:12:15 +0200

    Update StudentService.java

--- src/main/java/lms/service/StudentService.java
+++ src/main/java/lms/service/StudentService.java
@@ -1,6 +1,7 @@
 package lms.service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import lms.domain.Course;
 import lms.domain.CourseAttending;
+import lms.domain.Place;
 import lms.domain.Student;
 import lms.domain.StudentYear;
 import lms.domain.YearOfStudy;
@@ -21,17 +23,16 @@ public class StudentService {
 
 	@Autowired
 	StudentRepository studentRepository;
-	
+
 	@Autowired
 	YearOfStudyRepository yearOfStudyRepository;
-	
+
 	@Autowired
 	CourseAttendingRepository courseAttendingRepository;
-	
+
 	@Autowired
 	CourseRepository courseRepository;
-	
-	
+
 	public void enrollInFirstYear(Student student) {
 		YearOfStudy yearOfStudy = yearOfStudyRepository.findFirstByNumberOfYear(1);
 		StudentYear studentYear = new StudentYear();
@@ -39,36 +40,59 @@ public class StudentService {
 		student.getStudentYears().add(studentYear);
 		studentRepository.save(student);
 	}
-	
+
 	public Student findByCardNumber(String cardNumber) {
 		return studentRepository.findFirstByCardNumber(cardNumber);
 	}
-	
-	
-	public Set<Course> findAllCurrentCourses(Student student)
-	{
-		Set<Course> ret = new HashSet<>();
-		
-		for(StudentYear studentYear: student.getStudentYears())
-			for(Course course: studentYear.getYearOfStudy().getCourses())
-				ret.add(course);
-		
-		return ret;
-		
-		
-	}
-	
-	public Set<Course> findAllFinishedCourses(Student student)
-	{
-		Set<Course> ret = new HashSet<>();
-		
-		for(CourseAttending courseAttending: student.getCourseAttendings())
-			ret.add(courseAttending.getCourseRealization().getCourse());
-		
-		return ret;
-			
-	}
-	
 
+	public Set<Course> findAllCurrentCourses(Student student) {
+		Set<Course> ret = new HashSet<>();
+
+		for (StudentYear studentYear : student.getStudentYears())
+			for (Course course : studentYear.getYearOfStudy().getCourses())
+				ret.add(course);
+
+		return ret;
+
+	}
+
+	public Set<Course> findAllFinishedCourses(Student student) {
+		Set<Course> ret = new HashSet<>();
+
+		for (CourseAttending courseAttending : student.getCourseAttendings())
+			ret.add(courseAttending.getCourseRealization().getCourse());
+
+		return ret;
+
+	}
 	
+	public Iterable<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Optional<Student> getStudentById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    public void addStudent(Student s) {
+    	studentRepository.save(s);
+    }
+    
+    public Optional<Student> getStudent(Long id) {
+		return studentRepository.findById(id);
+	}
+
+	public void removeStudent(Long id) {
+		Optional<Student> s = studentRepository.findById(id);
+		studentRepository.delete(s.get());
+	}
+	
+	public void updateStudent(Long id, Student s) {
+		Optional<Student> S = studentRepository.findById(id);
+		if (S.isPresent()) {
+			s.setId(S.get().getId());
+			studentRepository.save(s);
+		}
+	}
+
 }
