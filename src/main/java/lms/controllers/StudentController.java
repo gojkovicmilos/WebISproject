5e161f25ab2682a73eaca47fd2141adccfe8f0aa
emil <emil@debian>
2019-05-10 01:01:05 +0200

    sc

--- src/main/java/lms/controllers/StudentController.java
+++ src/main/java/lms/controllers/StudentController.java
@@ -1,11 +1,21 @@
 package lms.controllers;
 
-import lms.service.StudentService;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+
+import lms.domain.Student;
+import lms.service.StudentService;
 
 @CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
@@ -14,6 +24,43 @@ public class StudentController {
 	
 	@Autowired
 	StudentService studentService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
+		Optional<Student> student = studentService.getStudentById(id);
+		if(student.isPresent()) {
+			return new ResponseEntity<Student>(student.get(), HttpStatus.OK);
+		}
+		return new ResponseEntity<Student>(HttpStatus.NOT_FOUND);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Student> addStudent(@RequestBody Student student) {
+		studentService.addStudent(student);
+		return new ResponseEntity<Student>(student, HttpStatus.CREATED);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student student) {
+		studentService.updateStudent(id, student);
+		return new ResponseEntity<Student>(student, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Student> deleteStudent(@PathVariable Long id) {
+		try {
+			studentService.removeStudent(id);
+			
+		}
+		catch(Exception e) {
+			return new ResponseEntity<Student>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Student>(HttpStatus.NO_CONTENT);
+		
+	}
+	
+	
+
 	
 	
 }
