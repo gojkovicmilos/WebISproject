d2810cb318a5ba08c6eacd4f3ea46d685bff3864
mgojkovic <33878577+mgojkovic@users.noreply.github.com>
2019-05-09 21:49:42 +0200

    Update StudentService.java
    svi trenutni predmeti i svi polozeni predmeti

--- src/main/java/lms/service/StudentService.java
+++ src/main/java/lms/service/StudentService.java
@@ -1,11 +1,18 @@
 package lms.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lms.domain.Course;
+import lms.domain.CourseAttending;
 import lms.domain.Student;
 import lms.domain.StudentYear;
 import lms.domain.YearOfStudy;
+import lms.repository.CourseAttendingRepository;
+import lms.repository.CourseRepository;
 import lms.repository.StudentRepository;
 import lms.repository.YearOfStudyRepository;
 
@@ -18,7 +25,14 @@ public class StudentService {
 	@Autowired
 	YearOfStudyRepository yearOfStudyRepository;
 	
-	public void enrolleInFirstYear(Student student) {
+	@Autowired
+	CourseAttendingRepository courseAttendingRepository;
+	
+	@Autowired
+	CourseRepository courseRepository;
+	
+	
+	public void enrollInFirstYear(Student student) {
 		YearOfStudy yearOfStudy = yearOfStudyRepository.findFirstByNumberOfYear(1);
 		StudentYear studentYear = new StudentYear();
 		studentYear.setYearOfStudy(yearOfStudy);
@@ -29,6 +43,32 @@ public class StudentService {
 	public Student findByCardNumber(String cardNumber) {
 		return studentRepository.findFirstByCardNumber(cardNumber);
 	}
+	
+	
+	public Set<Course> findAllCurrentCourses(Student student)
+	{
+		Set<Course> ret = new HashSet<>();
+		
+		for(StudentYear studentYear: student.getStudentYears())
+			for(Course course: studentYear.getYearOfStudy().getCourses())
+				ret.add(course);
+		
+		return ret;
+		
+		
+	}
+	
+	public Set<Course> findAllFinishedCourses(Student student)
+	{
+		Set<Course> ret = new HashSet<>();
+		
+		for(CourseAttending courseAttending: student.getCourseAttendings())
+			ret.add(courseAttending.getCourseRealization().getCourse());
+		
+		return ret;
+			
+	}
+	
 
 	
 }
