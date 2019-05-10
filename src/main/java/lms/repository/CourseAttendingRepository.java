46bc49f64acb0677c8e5257dde48c64049e313d9
Schmoopy93 <38230581+Schmoopy93@users.noreply.github.com>
2019-05-10 17:20:56 +0200

    Update CourseAttendingRepository.java

--- src/main/java/lms/repository/CourseAttendingRepository.java
+++ src/main/java/lms/repository/CourseAttendingRepository.java
@@ -3,10 +3,10 @@ package lms.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import lms.domain.ExamAttending;
+import lms.domain.CourseAttending;
 
 @Repository
-public interface CourseAttendingRepository extends JpaRepository<ExamAttending, Long>{
+public interface CourseAttendingRepository extends JpaRepository<CourseAttending, Long>{
 
 	
 
