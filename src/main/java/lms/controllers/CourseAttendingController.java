e92aa593e08190e90b434c29b5ce3ca9b125a8cf
Schmoopy93 <38230581+Schmoopy93@users.noreply.github.com>
2019-05-10 17:20:34 +0200

    Update CourseAttendingController.java

--- src/main/java/lms/controllers/CourseAttendingController.java
+++ src/main/java/lms/controllers/CourseAttendingController.java
@@ -1,10 +1,21 @@
 package lms.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lms.domain.CourseAttending;
 import lms.service.CourseAttendingService;
 
 @CrossOrigin(origins = { "http://localhost:4200" })
@@ -15,5 +26,41 @@ public class CourseAttendingController {
 	@Autowired
 	CourseAttendingService courseAttendingService;
 	
+	@RequestMapping()
+	public ResponseEntity<Iterable<CourseAttending>> getAllCourseAttending() {
+		return new ResponseEntity<Iterable<CourseAttending>>(courseAttendingService.getAllCourseAttending(), HttpStatus.OK);
+	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CourseAttending> getCourseAttendingId(@PathVariable Long id) {
+		Optional<CourseAttending> courseAttending = courseAttendingService.getCourseAttendingId(id);
+		if (courseAttending.isPresent()) {
+			return new ResponseEntity<CourseAttending>(courseAttending.get(), HttpStatus.OK);
+		}
+		return new ResponseEntity<CourseAttending>(HttpStatus.NOT_FOUND);
+	}
+
+	@PostMapping
+	public ResponseEntity<CourseAttending> addCourseAttending(@RequestBody CourseAttending courseAttending) {
+
+		courseAttendingService.addCourseAttending(courseAttending);
+		return new ResponseEntity<CourseAttending>(courseAttending, HttpStatus.CREATED);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CourseAttending> updateCourseAttending(@PathVariable Long id, @RequestBody CourseAttending courseAttending) {
+		courseAttendingService.updateCourseAttending(id, courseAttending);
+		return new ResponseEntity<CourseAttending>(courseAttending, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<CourseAttending> removeCourseAttending(@PathVariable Long id) {
+		try {
+			courseAttendingService.removeCourseAttending(id);
+		} catch (Exception e) {
+			return new ResponseEntity<CourseAttending>(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<CourseAttending>(HttpStatus.NO_CONTENT);
+	}
 }
