ea4ac989f0d167227a3083159b4ced251992d55b
Schmoopy93 <38230581+Schmoopy93@users.noreply.github.com>
2019-05-10 17:01:56 +0200

    Update CenterService.java

--- src/main/java/lms/service/CenterService.java
+++ src/main/java/lms/service/CenterService.java
@@ -1,8 +1,11 @@
 package lms.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lms.domain.Center;
 import lms.repository.CenterRepository;
 
 @Service
@@ -10,5 +13,29 @@ public class CenterService {
 	
 	@Autowired
 	CenterRepository centerRepository;
+	
+	public Iterable<Center> getAllCenter() {
+		return centerRepository.findAll();
+	}
+	
+	public void addCenter(Center c) {
+		centerRepository.save(c);
+	}
 
+	public Optional<Center> getCenterId(Long id) {
+		return centerRepository.findById(id);
+	}
+
+	public void removeCenter(Long id) {
+		Optional<Center> c = centerRepository.findById(id);
+		centerRepository.delete(c.get());
+	}
+
+	public void updateCenter(Long id, Center c) {
+		Optional<Center> cou = centerRepository.findById(id);
+		if (cou.isPresent()) {
+			c.setId(cou.get().getId());
+			centerRepository.save(c);
+		}
+	}
 }
