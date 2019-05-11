1100a54d27bdbb645790b91866c90c5263d06889
Schmoopy93 <38230581+Schmoopy93@users.noreply.github.com>
2019-05-11 03:25:37 +0200

    Update get-student.component.ts

--- wis-front/src/app/get-student/get-student.component.ts
+++ wis-front/src/app/get-student/get-student.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import Student from '../Student';
 import { StudentService } from '../student.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-get-student',
   templateUrl: './get-student.component.html',
@@ -10,7 +11,8 @@ export class GetStudentComponent implements OnInit {
 
   students: Student[];
 
-  constructor(private ss: StudentService) { }
+  constructor(private ss: StudentService,
+    private router: Router) { }
 
   ngOnInit() {
     this.ss
@@ -23,6 +25,7 @@ export class GetStudentComponent implements OnInit {
   deleteStudent(id) {
     this.ss.deleteStudent(id).subscribe(res => {
       console.log('Deleted');
+      this.router.navigate(['student/add']);
     });
   }
 }
