84306ada2866ae047fdee497c7b630fbe4cf7bb6
Schmoopy93 <38230581+Schmoopy93@users.noreply.github.com>
2019-05-12 18:55:29 +0200

    Update WebConfig.java

--- src/main/java/lms/config/WebConfig.java
+++ src/main/java/lms/config/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedMethods("DELETE");
+        .allowedMethods("DELETE","GET","POST","PUT");
     }
 }
