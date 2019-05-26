88990e51f0cab11c56bd61c85929ebc725b06cee
Schmoopy93 <38230581+Schmoopy93@users.noreply.github.com>
2019-05-26 02:34:03 +0200

    update

--- wis-front/src/app/home-page/home-page.component.ts
+++ wis-front/src/app/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import Center from '../Center';
 import { CenterService } from '../center.service';
 import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 
 @Component({
@@ -13,10 +14,12 @@ export class HomePageComponent implements OnInit {
 
   centers: Center[];
 
-  constructor(private router: Router, private cs: CenterService) { }
+  constructor(private dataService: DataService,private router: Router, private cnt: CenterService) { }
 
   ngOnInit() {
-    
+    this.cnt.getCenter()
+    .subscribe((data: Center[]) => {
+      this.centers = data;
+    });
   }
-
 }
