f8cdff22d35f78dab47779c73ea59152859e9ef5
emil <emil@debian>
2019-05-29 21:19:02 +0200

    plugin za kolokvijum

---
+++ src/main/java/lms/controllers/MessageController.java
@@ -0,0 +1,21 @@
+package lms.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lms.plugin.PluginRepository;
+
+@Controller
+@RequestMapping("/message")
+public class MessageController {
+	@Autowired
+	PluginRepository pr;
+	
+	@RequestMapping()
+	public ResponseEntity<String> getMessages() {
+		return pr.getPlugins("message").get(0).sendRequest(HttpMethod.GET, "http://localhost:8081/plugin/message");
+	}
+}
