08db39501925fca178fc009478a9ea5c4cefa014
mgojkovic <33878577+mgojkovic@users.noreply.github.com>
2019-06-08 18:43:54 +0200

    wis2k

--- src/main/java/lms/service/AdministratorService.java
+++ src/main/java/lms/service/AdministratorService.java
@@ -1,5 +1,6 @@
 package lms.service;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -8,17 +9,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lms.domain.Administrator;
+import lms.domain.Course;
+import lms.domain.CourseAttending;
 import lms.domain.Student;
 import lms.domain.StudentYear;
 import lms.domain.StudyProgram;
 import lms.domain.YearOfStudy;
 import lms.repository.AdministratorRepository;
+import lms.repository.StudentYearRepository;
+import lms.repository.YearOfStudyRepository;
 
 @Service
 public class AdministratorService {
 
 	@Autowired
 	AdministratorRepository administratorRepository;
+	
+	@Autowired
+	StudentYearRepository studentYearRepository;
+	
+	@Autowired
+	YearOfStudyRepository yearOfStudyRepository;
 	
 	public AdministratorService() {}
 
@@ -52,6 +63,8 @@ public class AdministratorService {
 		
 		YearOfStudy yearOfStudy = null;
 		
+		int pointsNeeded = yearNumber*48;
+		
 		for(YearOfStudy yos: studyProgram.getYearsOfStudy())
 			if(yos.getNumberOfYear() == yearNumber)
 			{
@@ -68,6 +81,83 @@ public class AdministratorService {
 		{
 			allStudentsOnYear.add(sy.getStudent());
 		}
+		
+		for(Student s: allStudentsOnYear)
+		{
+			int points = 0;
+			
+			for(CourseAttending ca: s.getCourseAttendings())
+			{
+				points+=ca.getCourseRealization().getCourse().getEcts();
+			}
+			
+			if(points>=pointsNeeded)
+			{
+				ret.add(s);
+			}
+		}
+		
+		
+		return ret;
+	}
+	
+	public boolean enrollStudentInNextYear(Student student, Set<Course>facultativeCourses)
+	{
+		boolean ret = true;
+		
+		boolean isIn = false;
+		
+		int currentYear = 0;
+		
+		StudyProgram studyProgram = null;
+		
+		for(StudentYear sy: student.getStudentYears())
+		{
+			currentYear++;
+			studyProgram = sy.getYearOfStudy().getStudyProgram();
+		}
+		
+		for(Student s: this.getAllStudentsWhoPass(studyProgram, currentYear))
+			if(s == student)
+				isIn = true;
+			
+		if(!isIn)
+			return false;
+		
+		
+		
+		YearOfStudy nextYOS = null;
+		
+		
+		for(YearOfStudy yos: yearOfStudyRepository.findAll())
+		{
+			if(yos.getStudyProgram() == studyProgram && yos.getNumberOfYear() == currentYear+1)
+				nextYOS = yos;
+		}
+		
+		for(Course c: facultativeCourses)
+			if(c.isObligatory())
+				return false;
+		
+	
+			Set<Course> newSet = nextYOS.getCourses();
+			
+			newSet.addAll(facultativeCourses);
+			
+			nextYOS.setCourses(newSet);
+		
+		StudentYear nextYear = new StudentYear(LocalDate.now(), nextYOS, student);
+		
+		Set<StudentYear> newYears = student.getStudentYears();
+		
+		newYears.add(nextYear);
+		
+		student.setStudentYears(newYears);
+		
+		
+		
+		
+		
 		
 		
 		return ret;
